4e02132e31481c05f1ce2f107ccfdd0b3c7d57d4
spdwal <568741362@qq.com>
2017-11-20 17:07:35 +0800

    1.1.13 prtint transposition of a two-dimensional array

---
+++ Chapter_1/practise/1_13/Transposition.hpp
@@ -0,0 +1,15 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+template<typename T>
+void printTransposition(const vector<vector<T>> &ivec)
+{
+    for(int i = 0; i < ivec[0].size(); ++i)
+    {
+        for(int j = 0; j < ivec.size(); ++j)
+        {
+            cout << ivec[j][i] << ' ';
+        }
+        cout << endl;
+    }
+}
---
+++ Chapter_1/practise/1_13/main.cpp
@@ -0,0 +1,12 @@
+#include"Transposition.hpp"
+#include<vector>
+using namespace std;
+
+int main()
+{
+	vector<vector<int>> ivec{{1,2,3,4,5},
+		{6,7,8,9,10}};
+	printTransposition(ivec);
+
+	return 0;
+}

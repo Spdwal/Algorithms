6114e93e7dffd39692cf199fdea574b1bdc9e064
spdwal <568741362@qq.com>
2017-11-29 15:42:33 +0800

    add a exception if num in lg < 0

--- Chapter_1/practise/1_14/lg.hpp
+++ Chapter_1/practise/1_14/lg.hpp
@@ -1,12 +1,17 @@
 #include<limits>
+#include<stdexcept>
 int lg(int num)
 {
-	for(int i = 1; i <= std::numeric_limits<int>::max(); ++i)
-	{
-		if(i*i > num)
-		{
-			return i-1;
-		}else
-			continue;
-	}
+    if(num < 0)
+    {
+        throw length_error("the num in lg() should >0");
+    }else
+        for(int i = 1; i <= std::numeric_limits<int>::max(); ++i)
+        {
+            if(i*i > num)
+            {
+                return i-1;
+            }else
+                continue;
+        }
 }

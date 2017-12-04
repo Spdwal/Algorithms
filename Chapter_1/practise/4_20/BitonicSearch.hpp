fa7b37b03b494b92baaf404fa7c9089a97a60095
spdwal <568741362@qq.com>
2017-12-04 11:22:09 +0800

    4_20 Bitonic search

--- Chapter_1/practise/4_20/BitonicSearch.hpp
+++ Chapter_1/practise/4_20/BitonicSearch.hpp
@@ -8,23 +8,29 @@
 #include<vector>
 #include<functional>
 using namespace std;
-tempalte<typename T, typename OP>
+
+
+template<typename T, typename OP>
 int BinarySearch(vector<T> &ivec, T val, int high, int low, OP op)
 {
     while(low <= high)
     {
         int mid = (low + high) / 2;
-        if(op(key, ivec[mid])){
+        if(op(val, ivec[mid])){
             high = mid - 1;
-        }else if(op(key, ivec[mid])){
+        }else if(op(ivec[mid], val)){
             low = mid + 1;
         }else
-            returm mid;
+            return mid;
     }
     return -1;
 }
-int BintonicSearchRank(vector<int> &ivec, int val, int high, int low)
+
+
+int BintonicSearchRank(vector<int> &ivec, int val)
 {
+    int low = 0;
+    int high = ivec.size() - 1;
     int index = -1;
 
     while(low <= high)
@@ -34,10 +40,10 @@ int BintonicSearchRank(vector<int> &ivec, int val, int high, int low)
         {
             return mid;
         }
-        if(ivec[mid] > a[mid - 1])
+        if(ivec[mid] > ivec[mid - 1])
         {
-            if(key < a[mid]){
-                index = BinarySearch(ivec, val, mid - 1, low, less);
+            if(val < ivec[mid]){
+	      index = BinarySearch(ivec, val, mid - 1, low, less<int>());
                 if(index != -1)
                 {
                     return index;
@@ -47,11 +53,11 @@ int BintonicSearchRank(vector<int> &ivec, int val, int high, int low)
         }
         if(ivec[mid] < ivec[mid - 1])
         {
-            if(key < a[mid]){
-                index = BinarySearch(ivec, val, high, mid + 1, greater);
+            if(val < ivec[mid]){
+	      index = BinarySearch(ivec, val, high, mid + 1, greater<int>());
             }
             if(index != -1){
-                return inxex;
+                return index;
             }
             high = mid - 1;
         }

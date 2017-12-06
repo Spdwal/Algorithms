62fab7b068ae00e87583e7f7d40cb329e1da15e5
spdwal <568741362@qq.com>
2017-12-06 16:42:47 +0800

    Union Find

---
+++ Chapter_1/UnionFind/main.cpp
@@ -0,0 +1,27 @@
+#include"UF.hpp"
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string filename;
+    cout << "Please enter the file name: ";
+    cin >> filename;
+    fstream fs(filename);
+    if(!fs.is_open()){
+        cerr << "fail to open the file " << filename << endl;
+        return 1;
+    }
+    int p, q;
+    UF uf(11);
+    while(fs >>p >> q){
+        if(uf.connected(p, q))
+            continue;
+        uf.Union(p, q);
+        cout << p << '-' << q << endl;
+    }
+    cout << uf.count() << endl;
+    return 0;
+}
